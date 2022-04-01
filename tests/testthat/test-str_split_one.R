9833da41c3e4ee55a93c8f3482698e926d2eb879
dliviya <dbilolikar@gmail.com>
2022-03-31 17:34:03 -0700

    tested and it ran

--- tests/testthat/test-str_split_one.R
+++ tests/testthat/test-str_split_one.R
@@ -1,8 +1,12 @@
-str_split_one <- function(string, pattern, n = Inf) {
-  stopifnot(is.character(string), length(string) <= 1)
-  if (length(string) == 1) {
-    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
-  } else {
-    character()
-  }
-}
+test_that("str_split_one() splits a string", {
+  expect_equal(str_split_one("a,b,c", ","), c("a", "b", "c"))
+})
+
+test_that("str_split_one() errors if input length > 1", {
+  expect_error(str_split_one(c("a,b","c,d"), ","))
+})
+
+test_that("str_split_one() exposes features of stringr::str_split()", {
+  expect_equal(str_split_one("a,b,c", ",", n = 2), c("a", "b,c"))
+  expect_equal(str_split_one("a.b", stringr::fixed(".")), c("a", "b"))
+})
